a51506066ab5229b92a7dcf1befa31b1ca3fe124
laurie <laurie.jane@kernology.com>
2022-09-10 12:03:58 -0500

    Add project files.

---
+++ ImageExtractor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageExtractor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
